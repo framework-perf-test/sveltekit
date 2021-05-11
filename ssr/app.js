f0d57378cdf477e508fd8d44d6c68b1a4d77c980
JeetChaudhari <JeetChaudhari@users.noreply.github.com>
2021-05-11 05:56:09 +0000

    deploy: 6b85fc44302f709015b074e63f53a92421fd3f25

--- ssr/app.js
+++ ssr/app.js
@@ -72,9 +72,9 @@ function init(settings) {
     amp: false,
     dev: false,
     entry: {
-      file: "/./_app/start-d5728a04.js",
+      file: "/./_app/start-4331c36a.js",
       css: ["/./_app/assets/start-a8cd1609.css"],
-      js: ["/./_app/start-d5728a04.js", "/./_app/chunks/vendor-1fa1ec0b.js"]
+      js: ["/./_app/start-4331c36a.js", "/./_app/chunks/vendor-1fa1ec0b.js"]
     },
     fetched: void 0,
     get_component_path: (id) => "/./_app/" + entry_lookup[id],
@@ -128,7 +128,7 @@ const module_lookup = {
     return index;
   })
 };
-const metadata_lookup = {"src/routes/$layout.svelte": {entry: "/./_app/pages/$layout.svelte-9352d073.js", css: [], js: ["/./_app/pages/$layout.svelte-9352d073.js", "/./_app/chunks/vendor-1fa1ec0b.js"], styles: null}, "src/routes/$error.svelte": {entry: "/./_app/pages/$error.svelte-69819081.js", css: [], js: ["/./_app/pages/$error.svelte-69819081.js", "/./_app/chunks/vendor-1fa1ec0b.js"], styles: null}, "src/routes/index.svelte": {entry: "/./_app/pages/index.svelte-ecd6e88b.js", css: [], js: ["/./_app/pages/index.svelte-ecd6e88b.js", "/./_app/chunks/vendor-1fa1ec0b.js"], styles: null}};
+const metadata_lookup = {"src/routes/$layout.svelte": {entry: "/./_app/pages/$layout.svelte-9352d073.js", css: [], js: ["/./_app/pages/$layout.svelte-9352d073.js", "/./_app/chunks/vendor-1fa1ec0b.js"], styles: null}, "src/routes/$error.svelte": {entry: "/./_app/pages/$error.svelte-69819081.js", css: [], js: ["/./_app/pages/$error.svelte-69819081.js", "/./_app/chunks/vendor-1fa1ec0b.js"], styles: null}, "src/routes/index.svelte": {entry: "/./_app/pages/index.svelte-b20137af.js", css: [], js: ["/./_app/pages/index.svelte-b20137af.js", "/./_app/chunks/vendor-1fa1ec0b.js"], styles: null}};
 async function load_component(file) {
   return {
     module: await module_lookup[file](),
@@ -232,6 +232,7 @@ ${``}
 				<td>${escape(todo.remind)}</td>
 				<td>${escape(todo.date)}</td>
 				<td><button type="${"button"}">Edit</button>
+					\xA0
 					<button type="${"button"}">Delete</button></td>
 			</tr>`)}</tbody></table>`;
 });
